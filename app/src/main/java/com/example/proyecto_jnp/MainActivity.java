7590727fb9e30aa9a812e56a764d17ef554869a4
“pol” <magicpolvillar@gmail.com>
2024-04-02 21:01:19 +0200

    commit pol 1 02/04/2024

--- app/src/main/java/com/example/proyecto_jnp/MainActivity.java
+++ app/src/main/java/com/example/proyecto_jnp/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.proyecto_jnp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button pruebaLogIn, pruebaRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        carga();
+
+        pruebaLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirLogIn();
+            }
+        });
+        pruebaRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirRegisterForm();
+            }
+        });
+    }
+
+    private void abrirLogIn() {
+        Intent intent = new Intent(this, LogIn_Screen.class);
+        startActivity(intent);
+        finish();
+    }
+    private void abrirRegisterForm() {
+        Intent intent = new Intent(this, RegisterForm_Screen.class);
+        startActivity(intent);
+        finish();
+    }
+    private void carga(){
+        pruebaLogIn = findViewById(R.id.pruebamain);
+        pruebaRegister = findViewById(R.id.pruebaregister);
     }
 }
---
+++ app/src/main/java/com/example/proyecto_jnp/RegisterForm_Screen.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_jnp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterForm_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_form_screen);
+    }
+}

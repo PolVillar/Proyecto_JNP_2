de04a04a91d330a479f6e5bd1f17df620718f3b9
“pol” <magicpolvillar@gmail.com>
2024-04-03 21:03:13 +0200

    commit pol 2 03/04/2024

--- app/src/main/java/com/example/proyecto_jnp/LogIn_Screen.java
+++ app/src/main/java/com/example/proyecto_jnp/LogIn_Screen.java
@@ -3,12 +3,18 @@ package com.example.proyecto_jnp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class LogIn_Screen extends AppCompatActivity {
 
+    private ImageView closet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_screen);
+
+        closet = findViewById(R.id.imageCloset);
+
+        closet.setImageResource(R.drawable.closet_img);
     }
 }
--- app/src/main/java/com/example/proyecto_jnp/RegisterForm_Screen.java
+++ app/src/main/java/com/example/proyecto_jnp/RegisterForm_Screen.java
@@ -3,12 +3,22 @@ package com.example.proyecto_jnp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewTreeObserver;
+import android.widget.EditText;
 
 public class RegisterForm_Screen extends AppCompatActivity {
 
+    private EditText firstName, lastName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_form_screen);
+
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+
+
+
     }
+
 }

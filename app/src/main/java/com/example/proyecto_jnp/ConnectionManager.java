3ec53bdf03ec70beb2ff982b5afaf1d1d4cd31a7
“pol” <magicpolvillar@gmail.com>
2024-06-03 13:36:37 +0200

    pushes finales 1.1

--- app/src/main/java/com/example/proyecto_jnp/ConnectionManager.java
+++
@@ -1,9 +0,0 @@
-package com.example.proyecto_jnp;
-
-public class ConnectionManager {
-    private static String url = "https://192.168.18.12:8443/users/authenticate";//"https://192.168.8.145/swagger-ui.html";
-
-    public static String getUrl() {
-        return url;
-    }
-}
--- app/src/main/java/model/OnItemClickListener.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public interface OnItemClickListener {
-    void onItemClick(int pos);
-}
